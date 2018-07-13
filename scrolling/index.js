aad68944fd504a8772a9864b229e868083fa4aed
Martin Banks <anotherbanksy@gmail.com>
2018-07-13 21:32:56 +1000

    adds model and and scroll transtion

--- scrolling/index.js
+++ scrolling/index.js
@@ -1,8 +1,61 @@
+// Create Scene
+const scene = new THREE.Scene()
+// create camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+camera.position.z = 200
+
+// Create renderer
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+})
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setClearColor('#fff')
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+// Add lights
+const keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 100%)'), 0.3)
+keyLight.position.set(0, 200, 1000)
+scene.add(keyLight)
+
+const ambient = new THREE.AmbientLight( '#333', 0.3 ) // soft white light
+scene.add( ambient )
+
+const fillLight = new THREE.DirectionalLight('#fff', 1)
+fillLight.position.set(100, 50, 100)
+scene.add(fillLight)
+
+const backLight = new THREE.DirectionalLight(0xffffff, 1)
+backLight.position.set(-50, 0, -100).normalize()
+scene.add(backLight)
+
 const app = document.createElement('div')
 app.id = 'app'
 document.body.appendChild(app)
+
 const pre = document.createElement('pre')
 document.body.appendChild(pre)
+
+const model = document.createElement('div')
+model.id = 'model'
+document.body.appendChild(model)
+
+const mtlLoader = new THREE.MTLLoader()
+mtlLoader.setPath('./model/')
+mtlLoader.load('Small_Tropical_Island.mtl', material => {
+  material.preload()
+  const objLoader = new THREE.OBJLoader()
+  objLoader.setMaterials(material)
+  objLoader.setPath('./model/')
+  objLoader.load('Small_Tropical_Island.obj', object => {
+    scene.add(object)
+  })
+})
+
 
 const dumpContent = {
   sections: [],
@@ -19,10 +72,16 @@ const makeSections = [...new Array(5)]
   })
   .join('')
 
+function pars () {
+  return `<div class="pars">
+    ${[...new Array(5)].map(p => `<p>${lorem}</p>`).join('')}
+  </div>`
+}
+
 const page = [
-  [...new Array(5)].map(p => `<p>${lorem}</p>`).join(''),
+  pars(),
   makeSections,
-  [...new Array(5)].map(p => `<p>${lorem}</p>`).join(''),
+  pars(),
 ].join('')
 
 app.innerHTML = page
@@ -44,31 +103,78 @@ const updateDump = {
   },
   scroll () {
     dumpContent.scrollY = window.scrollY
+  },
+  camera () {
+    dumpContent.camera = {
+      position : camera.position,
+      rotation: camera.rotation,
+    }
   }
 }
 
+camera.position.y = 0
+camera.position.x = 0
+camera.position.z = 0
+
+camera.rotation.x = 0
+camera.rotation.y = 0
+camera.rotation.z = 0
+
+function angle (degree) {
+  return degree*(Math.PI/180)
+}
+
+
+
 const mods = [
+  // for every tick set all values of camera rotation.position
+  // that can be changed for accurate update/reset
   {
     func (el, pct) {
-      e.style.opacity = 1 - pct
+      el.style.color = `rgb(${255 * pct},0,0)`
+      camera.position.y = 110 * pct
+      camera.position.x = 100 * pct
+      camera.rotation.y = 0.5 * pct
+    }
+  },
+  {
+    func (el, pct) {
+      camera.position.z = (200 * pct)
+      camera.rotation.y = angle(180) * pct
+    }
+  },
+  {
+    func (el, pct) {
+      camera.position.z = 100 - (100 * pct)
     }
   }
 ]
 
 function handleScroll (e) {
   updateDump.scroll()
-  sections.forEach(updateDump.section)
+  updateDump.camera()
 
-  sections.forEach(s => {
+  sections.forEach((s, i) => {
+    updateDump.section(s, i)
     const { top, height } = s.getBoundingClientRect()
     if (top < 0 && top > (0 - window.innerHeight)) {
-      s.querySelector('h1').innerText = `Active: ${window.scrollY}px`
-      const pct = 0
-      mods[0].func(s, pct)
+      s.querySelector('h1').innerText = `Section ${i} is active: ${top}px`
+      const pct = Math.sqrt(Math.pow((top / height), 2))
+      console.log(pct)
+      if (mods[i]) {
+        mods[i].func(s, pct)
+      }
     }
   })
 
   dump()
 }
 
+function animate () {
+  // controls.update()
+  renderer.render(scene, camera)
+  requestAnimationFrame(animate)
+}
+document.body.appendChild(renderer.domElement)
+animate()
 window.addEventListener('scroll', handleScroll)
