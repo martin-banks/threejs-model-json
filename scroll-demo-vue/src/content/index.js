15519db88ba8fbf232f04bb6a5d1bdfd0770f42b
martin-banks <anotherbanksy@gmail.com>
2018-07-18 19:34:46 +1000

    adds sample content with inline obj/model

--- scroll-demo-vue/src/content/index.js
+++ scroll-demo-vue/src/content/index.js
@@ -1,4 +1,5 @@
 // DEMOS
-import content from '@/content/projects/simple3d'
+// import content from '@/content/projects/simple3d'
+import content from '@/content/projects/inline_test'
 
 export default content
