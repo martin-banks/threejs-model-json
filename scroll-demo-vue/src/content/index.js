53de7e3f9a15e21cba7c32729a30a8640db16eb5
martin-banks <anotherbanksy@gmail.com>
2018-07-24 18:21:53 +1000

    new content for scroll maghic test

--- scroll-demo-vue/src/content/index.js
+++ scroll-demo-vue/src/content/index.js
@@ -1,5 +1,6 @@
 // DEMOS
 // import content from '@/content/projects/simple3d'
-import content from '@/content/projects/inline_test'
+// import content from '@/content/projects/inline_test'
+import content from '@/content/projects/scroll_test'
 
 export default content
